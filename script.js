23252eb72e4b8d25f0a9dcab020d2261c429f0d4
anish <anishdutta012@gmail.comm>
2023-07-12 14:49:58 +0530

    products in home

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ $(document).ready(function(){
       $('nav').removeClass('black')
     }
   })
+
+  $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
