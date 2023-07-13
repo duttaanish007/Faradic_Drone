27da8948752b81bdd62b2a13222dfe9bcf0d08c6
anish <anishdutta012@gmail.comm>
2023-07-13 11:23:11 +0530

    GALLERY AND TEAM UPDATED

--- gallery.js
+++ gallery.js
@@ -1,36 +1,4 @@
-
-//Menu toggle-effect
-$(document).ready(function(){
-    $(".menu-icon").on("click",function(){
-      $("nav ul").toggleClass("showing");
-    });
-  });
-  
-  //Scrolling Effect
-  $(window).on('scroll', function(){
-    if($(window).scrollTop()) {
-      $('nav').addClass('black');
-    }
-    else{
-      $('nav').removeClass('black')
-    }
-  })
-
-  $(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
-  });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
-  
-
-
-
-  const galleryItem = document.getElementsByClassName("gallery-item");
+const galleryItem = document.getElementsByClassName("gallery-item");
 const lightBoxContainer = document.createElement("div");
 const lightBoxContent = document.createElement("div");
 const lightBoxImg = document.createElement("img");
